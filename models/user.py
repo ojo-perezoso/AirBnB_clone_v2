dad0465d3e3e604619a6115d2ee816b313733a20
ojo-perezoso <bymartin24@gmail.com>
2022-04-12 14:26:22 -0700

    merging with right lat and long

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from os import environ as env
 
 
 class User(BaseModel, Base):
@@ -13,5 +13,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user',
-                          cascade='all, delete-orphan')
+    if env.get('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship('Place',
+                              backref='user',
+                              cascade='all, delete-orphan')
