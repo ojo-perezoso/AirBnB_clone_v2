729d409f1e5f66ed5ef6e818159b50129acf3a88
ojo-perezoso <bymartin24@gmail.com>
2022-04-12 13:55:02 -0700

    fix: add places-cities relationship

--- models/city.py
+++ models/city.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import environ as env
 from models.base_model import BaseModel, Base
 from models.state import State
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -11,3 +13,7 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if env.get('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship('places',
+                               backref='cities',
+                               cascade='all, delete_orphans')
