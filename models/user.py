f303900d4afa5dee56d39b8fb7c9c06c7a5160a0
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-12 14:00:14 -0700

    updated user. added relationship with theri own city

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
@@ -11,3 +13,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    if (getenv('HBNB_ENV') == 'db')
+        places = relationship('Place', backref='user',
+                              cascade='all, delete-oprphan')
