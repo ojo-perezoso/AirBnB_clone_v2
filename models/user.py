43d9cf1e4672c8245ac48c6298e8aa5a7b12e0b6
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-12 14:21:51 -0700

    ?

--- models/user.py
+++ models/user.py
@@ -13,6 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    if (getenv('HBNB_ENV') == 'db'):
-        places = relationship('Place', backref='user',
-                              cascade='all, delete-orphan')
+    places = relationship('Place', backref='user',
+                          cascade='all, delete-orphan')
