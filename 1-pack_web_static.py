a1c6782097bd53b7b1c82f77c3f27276e54ac4e3
ojo-perezoso <bymartin24@gmail.com>
2022-04-29 02:10:57 +0000

    add: first use of fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,10 +4,18 @@ from os import path
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """Turns the web_static forlder into a .tgz file"""
-    if path.isdir('web_static'):
-        local('sudo mkdir versions')
-        cmd = "tar -cfzv versions/web_static_$(date '+%Y-%m-%d-%H-%M-%s').tgz\
-                web_static"
+    f = 'web_static'
+    if path.isdir(f):
+        d = datetime.now()
+        cmd = 'tar -cfzv versions/web_static_{}{}{}{}{}{} {}'.format(
+                d.year,
+                d.month,
+                d.day,
+                d.hour,
+                d.minute,
+                d.second,
+                f)
         return(local(cmd))
